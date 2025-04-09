c2b42c22d6397c093d2feb0a1c2589119ddf0af1
sol <146923236+jinsol24@users.noreply.github.com>
2025-04-09 10:45:56 +0900

    Update widget.cpp

--- Sources/widget.cpp
+++ Sources/widget.cpp
@@ -16,7 +16,6 @@ Widget::~Widget()
     delete ui;
 }
 
-// [조언] 500원 추가가
 // 버튼 상태 업데이트
 void Widget::updateButtons() {
     ui->pbTea->setEnabled(money >= 150);
@@ -35,28 +34,65 @@ void Widget::changeMoney(int diff) {
 
 // 잔돈 반환 멘트 생성
 QString Widget::calculateChange(int money) {
- 기
-    // if(money >= 150) {
-    //     changeMoney(-150);
-    // } else {
-    //     QMessageBox::warning(this, "경고", "잔액이 부족합니다.");
-    // }
+    int coin500 = money / 500;
+    money %= 500;
+    int coin100 = money / 100;
+    money %= 100;
+    int coin50 = money / 50;
+    money %= 50;
+    int coin10 = money / 10;
+    money %= 10;
+
+    return QString("500원 %4개, 100원 %1개, 50원 %2개, 10원 %3개 반환됩니다.")
+        .arg(coin100)
+        .arg(coin50)
+        .arg(coin10)
+        .arg(coin500);
+}
+
+void Widget::on_pb10_clicked()
+{
+    changeMoney(10);
+}
+
+void Widget::on_pb50_clicked()
+{
+    changeMoney(50);
+}
+
+void Widget::on_pb100_clicked()
+{
+    changeMoney(100);
+}
+
+void Widget::on_pb500_clicked()
+{
+    changeMoney(500);
+}
+
+void Widget::on_pbReset_clicked()
+{
+    QMessageBox::information(this, "잔돈 반환", calculateChange(money));
+    changeMoney(-money);
+    // money = 0;
+    // ui->lcdNumber->display(money);
+    // updateButtons();
+}
+
+void Widget::on_pbTea_clicked()
+{
+    assert(money >= 150) ;
+    changeMoney(-150);
 }
 
 void Widget::on_pbJuice_clicked()
 {
-    if(money >= 200) {
-        changeMoney(-200);
-    } else {
-        QMessageBox::warning(this, "경고", "잔액이 부족합니다.");
-    }
+    assert(money >= 200);
+    changeMoney(-200);
 }
 
 void Widget::on_pbCoffee_clicked()
 {
-    if(money >= 100) {
-        changeMoney(-100);
-    } else {
-        QMessageBox::warning(this, "경고", "잔액이 부족합니다.");
-    }
+    assert(money >= 100);
+    changeMoney(-100);
 }
